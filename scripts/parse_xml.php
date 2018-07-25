ce733929d9971f6335f30ee88c887ee667df0421
Richard Secor <rsecor@rsecor.com>
2018-07-25 11:00:02 -0400

    Minor changes.

--- scripts/parse_xml.php
+++ scripts/parse_xml.php
@@ -3,16 +3,24 @@
 class parse_xml
 {
 
-	function __construct ( $dir )
+	public function __construct ( $dir )
 	{
+		return ( TRUE ) ;
 	}
 
-	function __deconstruct ( )
+	public function __deconstruct ( )
 	{
+		return ( TRUE ) ;
 	}
 
-	function socket_read ( $data )
+	public function socket_read ( $data )
 	{
+		$dom = new DOMDocument ( ) ;
+		@$dom -> loadHTML ( $data ) ;
+		foreach ( $dom -> getElementsByTagName ( '*' ) as $tag_no => $tag_info )
+		{
+			print $tag_no . " has " . print_r ( $tag_info , TRUE ) ;
+		}
 		$data_out = "parsed: " . $data ;
 		return ( $data_out ) ;
 	}

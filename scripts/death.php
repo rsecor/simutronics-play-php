8462a7a9ee388fc353cd476c6a6f5d29e8f62c95
Richard Secor <rsecor@rsecor.com>
2018-08-14 14:45:35 -0400

    Various changes to get location out of the messages.

--- scripts/death.php
+++ scripts/death.php
@@ -6,6 +6,32 @@ class death
 	public function __construct ( $socket , $dir )
 	{
 		$this -> { 'socket' } = $socket ;
+
+		// INFO - https://gswiki.play.net/Death%27s_Sting#Death_Messaging
+		$this -> { 'location' } [ "(.*) just got squashed!" ] = "Cysaegir" ;	
+		$this -> { 'location' } [ "(.*) has gone to feed the fishes!" ] = "River's Rest / Citadel" ;
+		$this -> { 'location' } [ "(.*) just bit the dust!" ] = "Wehnimer's Landing" ;
+		$this -> { 'location' } [ "(.*) just turned her last page!" ] = "Ta'Illistim / OTF" ;
+		$this -> { 'location' } [ "(.*) was just put on ice!" ] = "Icemule Trace" ;
+		$this -> { 'location' } [ "(.*) just punched a one-way ticket!" ] = "Teras Isle / Ruined Temple" ;
+		$this -> { 'location' } [ "(.*) is going home on his shield!" ] = "Ta'Vaalor" ;
+		$this -> { 'location' } [ "(.*) just took a long walk off a short pier!" ] = "Solhaven" ;
+		$this -> { 'location' } [ "(.*) is dust in the wind!" ] = "Four Winds Isle" ;
+		$this -> { 'location' } [ "(.*) is six hundred feet under!" ] = "Zul Logoth" ;
+		$this -> { 'location' } [ "(.*) may just be going home on his shield!" ] = "Aradhul Road / Displaced Red Forest" ;
+		$this -> { 'location' } [ "The death cry of (.*) echoes in your mind!" ] = "The Rift" ;
+		$this -> { 'location' } [ "(.*) (.*) in the Elemental Confluence!" ] = "Elemental Confluence" ;
+		$this -> { 'location' } [ "(.*) just gave up the ghost!" ] = "Castle Varunar / Trail to Solhaven / Trail to Icemule" ;
+		$this -> { 'location' } [ "(.*)'s flame just burnt out in the Sea of Fire!" ] = "Sanctum of Scales" ;
+		$this -> { 'location' } [ "(.*) just lost his way somewhere in the Settlement of Reim!" ] = "Reim" ;
+		$this -> { 'location' } [ "(.*) just perished defending a fortress within Reim!" ] = "Reim Fortress Defense" ;
+		$this -> { 'location' } [ "(.*) just defeated in Duskruin Arena!" ] = "Duskruin Arena" ;
+		$this -> { 'location' } [ "(.*) just perished underneath Bloodriven Village!" ] = "Duskruin Arena Sewers" ;
+		$this -> { 'location' } [ "(.*) failed within the Bank at Bloodriven!" ] = "Duskruin Bank Heist" ;
+		$this -> { 'location' } [ "(.*) is now fish food for the fauna of Caligos Isle!" ] = "Ebon Gate Festival - Caligos Isle" ;
+		$this -> { 'location' } [ "(.*) was just reunited with her ancestors!" ] = "Ebon Gate Festival - Feywrot Mire" ;
+		$this -> { 'location' } [ "(.*) failed to bring a shrubbery to the Night at the Academy!" ] = "Night at the Academy" ;
+
                 if ( ! ( file_exists ( $dir [ 'logs' ] ) ) )
                 {
                         mkdir ( $dir [ 'logs' ] ) ;
@@ -29,13 +55,56 @@ class death
 	{
 		if ( preg_match ( "/id=\"death\"/i" , $buf ) )
 		{
-			if ( preg_match ( '/noun="(.*)"/i' , $buf , $matches ) )
+			if ( $split1 = preg_split ( "/<a /i" , $buf ) )
 			{
-				if ( isset ( $matches [ 1 ] ) )
+				if ( isset ( $split1 [ 1 ] ) )
 				{
-					$character_name = $matches [ 1 ] ;
-					print '[' . __CLASS__ . ']: ' . date ( "Ymd H:i:s" ) . ": " . $character_name . "\n" ;
-			                file_put_contents ( $this -> { 'log' } , date ( "Ymd-His" ) . ": " . $character_name . "\n" , FILE_APPEND ) ;
+					if ( preg_match_all ( '/ noun="(.*)">(.*)<\/a>/i' , $split1 [ 1 ] , $matches ) )
+					{
+						if ( $matches [ 1 ] [ 0 ] == $matches [ 2 ] [ 0 ] )
+						{
+							$character_name = $matches [ 1 ] [ 0 ] ;
+						}
+					}	
+				}
+			}
+			foreach ( $this -> { 'location' } as $death_message => $location )
+			{
+				if ( preg_match_all ( "/" . $death_message . "/i" , $buf , $matches ) )
+				{
+					if ( isset ( $matches [ 1 ] ) )
+					{
+						$death_location = $location ;
+						break ;
+					}
+				}
+			}
+			if ( isset ( $character_name ) )
+			{
+				if ( isset ( $death_location ) )
+				{
+					print '[' . __CLASS__ . ']: ' . $character_name . ' has died near ' . $death_location . "\n" ;
+			                file_put_contents ( $this -> { 'log' } , date ( "Ymd-His" ) . ": " . $character_name . ":" . $death_location . "\n" , FILE_APPEND ) ;
+				}
+				else
+				{
+					print $buf ;
+			                file_put_contents ( $this -> { 'log' } , date ( "Ymd-His" ) . ": " . $character_name . ": UNKNOWN\n" , FILE_APPEND ) ;
+				}
+			}
+			else
+			{
+				if ( isset ( $death_location ) )
+				{
+					print $buf ;
+					print '[' . __CLASS__ . ']: ' . 'UNKNOWN' . ' has died near ' . $death_location . "\n" ;
+			                file_put_contents ( $this -> { 'log' } , date ( "Ymd-His" ) . ": " . 'UNKNOWN' . ":" . $death_location . "\n" , FILE_APPEND ) ;
+				}
+				else
+				{
+					print $buf ;
+					print '[' . __CLASS__ . ']: ' . 'UNKNOWN' . ' has died near ' . 'UNKNOWN' . "\n" ;
+			                file_put_contents ( $this -> { 'log' } , date ( "Ymd-His" ) . ": " . 'UNKNOWN' . ": UNKNOWN\n" , FILE_APPEND ) ;
 				}
 			}
 		}
